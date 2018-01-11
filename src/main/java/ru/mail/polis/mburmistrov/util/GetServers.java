38db1d9c1192136577795c9f3eb1c49cc4cf1974
Mikhail Burmistrov <misha.burmistrov@gmail.com>
2018-01-11 21:11:04 +0300

    Add cache to GetServers class

--- src/main/java/ru/mail/polis/mburmistrov/util/GetServers.java
+++ src/main/java/ru/mail/polis/mburmistrov/util/GetServers.java
@@ -3,18 +3,32 @@ package ru.mail.polis.mburmistrov.util;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GetServers {
+    private static final Map<String, List<String>> cache;
+
+    static {
+        cache = new HashMap<>();
+    }
+
     public static List<String> getNodesById(@NotNull String id,
                                             int from,
                                             @NotNull List<String> topology) {
+        if (cache.containsKey(id + from)) {
+            return cache.get(topology + id + from);
+        }
+
         List<String> nodes = new ArrayList<>();
         int hash = Math.abs(id.hashCode());
         for (int i = 0; i < from; i++) {
             int idx = (hash + i) % topology.size();
             nodes.add(topology.get(idx));
         }
+
+        cache.put(topology + id + from, nodes);
         return nodes;
     }
 }
