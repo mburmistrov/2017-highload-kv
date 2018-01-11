22f2c9b43cf348dab1d77978fa4ed538587d03a4
Mikhail Burmistrov <misha.burmistrov@gmail.com>
2018-01-11 20:56:03 +0300

    Do not use multithreading for delete and add cache in BasicStorage

--- src/main/java/ru/mail/polis/mburmistrov/storage/BasicStorage.java
+++ src/main/java/ru/mail/polis/mburmistrov/storage/BasicStorage.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -14,20 +16,29 @@ public class BasicStorage implements Storage {
 
     private final String dir;
     private final Executor exec;
+    private final Map<String, byte[]> cache;
 
     public BasicStorage(String dir) {
         this.dir = dir;
         exec = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        cache = new HashMap<>();
     }
 
     @NotNull
     @Override
     public byte[] get(@NotNull String id) throws NoSuchElementException, IllegalArgumentException, IOException {
+        if (cache.containsKey(id)) {
+            return cache.get(id);
+        }
+
         Path path = Paths.get(dir, id);
         if (!Files.exists(path)) {
             throw new NoSuchElementException("Invalid ID: " + id);
         }
-        return Files.readAllBytes(Paths.get(dir, id));
+
+        byte[] data = Files.readAllBytes(Paths.get(dir, id));
+        cache.put(id, data);
+        return data;
     }
 
     @Override
@@ -35,6 +46,8 @@ public class BasicStorage implements Storage {
         // это может ускорить вставку данных, но мы можем получить
         // java.nio.file.NoSuchFileException когда быстро обращаемся к
         // вставляемому файлу
+
+        cache.remove(id);
 
         //exec.execute(() -> {
         try {
@@ -47,13 +60,17 @@ public class BasicStorage implements Storage {
 
     @Override
     public void delete(@NotNull String id) throws IllegalArgumentException, IOException {
-        exec.execute(() -> {
+        cache.remove(id);
+
+        // аналогичная ситуация как и с upsert
+
+        //exec.execute(() -> {
             try {
                 Files.deleteIfExists(Paths.get(dir, id));
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        });
+        //});
     }
 
 }
